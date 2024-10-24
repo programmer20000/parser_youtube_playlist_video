c6ad5abe8d4cea814556c0184b246e30cf7ff2ce
Mihai Apostol <mihaiapostol153@gmail.com>
2024-10-24 11:59:55 +0300

    add test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+def create_file_from_list(self, filename='', data_list=None, mode='w'):
+    """
+            Create a file from a list of strings.
+
+            Parameters:
+            filename (str): The name of the file to be created.
+            data_list (list of str): The list of strings to write to the file.
+            """
+
+
